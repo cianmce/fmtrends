0c5b985d8cf2e369878d26946211a04298200e3a
Cian McElhinney <cianmce@gmail.com>
2016-06-01 18:47:30 +0100

    clock

--- clock.py
+++ clock.py
@@ -1,13 +1,28 @@
 import time
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
 def timed_job():
-    print '\n\n\tThis job is run every 5 minutes.\n\n'
+    print 'Doing stuff every 3 mins'
+    time.sleep(30)
+    print 'Done'
 
 
-def main():
-    timed_job()
-    time.sleep(5*60)
-    timed_job()
+sched.start()
 
-if __name__ == '__main__':
-    main()
+
+
+# def timed_job():
+#     print '\n\n\tThis job is run every 5 minutes.\n\n'
+
+
+# def main():
+#     timed_job()
+#     time.sleep(5*60)
+#     timed_job()
+
+# if __name__ == '__main__':
+#     main()
